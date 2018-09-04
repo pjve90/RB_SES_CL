b7a113e43ec7c5f9f5915b241668e6212e76c004
pjve90 <pjvarase@gmail.com>
2018-09-04 14:56:46 +0200

    updated code for the statistical analyses

--- Replicability script.R
+++ Replicability script.R
@@ -7,9 +7,9 @@
 getwd()
 setwd("folder_directory")
 #Establishing history and image
-savehistory("folder_directory")
-save.image("folder_directory")
-load("folder_directory")
+savehistory(".~RB_SES_CL.Rhistory")
+save.image(".~RB_SES_CL.RData")
+load(".~RB_SES_CL.RData")
 #Checking memory usage
 memory.limit()
 memory.size(max = FALSE)
@@ -27,7 +27,7 @@ library(dplyr)
 #Importing CASEN database
 
 #importing CASEN with spss.get()
-casen <- spss.get("C:/Users/Pablo/Dropbox/Publicacion/RB_SES_CL/CASEN_2013_MN_B_Principal.sav", use.value.labels=TRUE)
+casen <- spss.get("folder_directory", use.value.labels=TRUE)
 #corroboration that is a data frame
 is.data.frame(casen)
 
@@ -76,9 +76,6 @@ sum(is.na(mc$s5))
 sum(is.na(mc$s6))
 namc <- mc[complete.cases(mc[,151:152]),]
 
-#Selection of childless women
-shm <- mc[which(mc$s5 ==0),]
-
 #Children selection of mononuclear households
 #children of both partners in the household selection
 haun <- unnucleo[which(unnucleo$pco1 == "Hijo(a) de ambos"),]
@@ -91,6 +88,9 @@ hpun <- unnucleo[which(unnucleo$pco1 == "Hijo(a) s?lo del esposo(a) o pareja"),]
 sum(is.na(hpun$pco1))
 #combination of children selections
 hun <- rbind(hjun, haun, hpun)
+
+#Selection of children of women in the cohort
+hmc <- hun[which((hun$folio %in% namc$folio) == TRUE),]
 
 #Selection of youngest children of women in the cohort
 tblhmc <- tbl_df(hmc)
@@ -160,11 +160,27 @@ nonamceuhien <- ccmceuhien[which(ccmceuhien$ien >= 0),]
 sum(is.na(nonamceuhien$euh))
 sum(is.na(nonamceuhien$ien))
 #Selection of Chilean women from the cohort
-chilesolo <- nonamceuhien[which(nonamceuhien$r1a == "Chilena (exclusivamente)"),]
-chiledoble <- nonamceuhien[which(nonamceuhien$r1a == "Chilena y otra (doble nacionalidad)"),]
+chilesolo1 <- nonamceuhien[which(nonamceuhien$r1a == "Chilena (exclusivamente)"),]
+chiledoble1 <- nonamceuhien[which(nonamceuhien$r1a == "Chilena y otra (doble nacionalidad)"),]
+
+#Selection of childless women
+shm <- mc[which(mc$s5 ==0),]
+shm$s5[which(shm$s5 == "No sabe / No recuerda")] <- NA
+shm$s5[which(shm$s5 == "No sabe")] <- NA
+summary(shm$s5)
+is.factor(shm$s5)
+shm$s5 <- as.numeric(levels(shm$s5))[shm$s5]
+is.numeric(shm$s5)
+is.factor(shm$s6)
+shm$s6 <- as.numeric(levels(shm$s6))[shm$s6]
+is.numeric(shm$s6)
+shm$euh <- NA
+shm$ien <- NA
+chilesolo2 <- shm[which(shm$r1a == "Chilena (exclusivamente)"),]
+chiledoble2 <- shm[which(shm$r1a == "Chilena y otra (doble nacionalidad)"),]
 
 #Final cohort database
-mcch <- rbind(chilesolo, chiledoble)
+mcch <- rbind(chilesolo1, chiledoble1,chilesolo2, chiledoble2)
 sum(is.na(mcch$edad))
 summary(mcch$edad)
 sum(is.na(mcch$s5))
@@ -179,9 +195,9 @@ summary(mcch$ien)
 #2.- Socioeconomic groups
 
 #R packages to use
-#Package "ca"
-install.packages("ca")
-library(ca)
+#Package FactoMineR
+install.packages("FactoMineR")
+library(FactoMineR)
 
 #Socioeconomic groups generation
 #NA management
@@ -220,23 +236,23 @@ cats <- apply(factor, 2, function(x) nlevels(as.factor(x)))
 
 #Multiple correspondance analysis
 #MCA with socioeconomic variables
-mca1 <- mjca(factor[,2:15], lambda = "indicator")
-plot(mca1, labels = 0, col = c("black", rainbow(15)), pch=15)
-legend(-37, 58, ncol = 2, legend = factor(colnames(factor[,2:15])), col = rainbow(15), pch=15)
-summary(mca1, scree = TRUE, rows = FALSE, columns = FALSE)
-
-#MCA with selected socioeconomic variables
-factor2 <- factor[,c("folio", "educ", "decilt", "decilpc", "s14", "v1", "v2", "v4", "v6", "v9", "v12")]
+mca1 <- MCA(factor[,2:15], ncp=5, axes = c(1,2))
+summary(mca1)
+sum(test$var$eta2[,1])/14
+test$var$eta2[,1]
+#MCA with selected socioeconomic variables (equal or higher than the mean contribution in dim 1)
+factor2 <- factor[,c("folio", "educ", "decilt", "decilpc", "s14", "v2", "v9", "v12")]
 cats2 <- apply(factor2, 2, function(x) nlevels(as.factor(x)))
-mca3 <- mjca(factor2[,2:11], lambda = "indicator")
-png("MCA.png", width = 4.9, height = 5.7, units = "in", res = 500)
-plot(mca3,  labels = 0, col = c("black", gray(0:10/10)), pch=15)
-legend(4.6, -43, ncol = 2, legend = factor(colnames(factor2[,2:11])), col = gray(0:10/10), pch=15, cex = 0.75)
+mca2 <- MCA(factor2[,2:8], ncp = 5, axes = c(1,2))
+pdf("MCA.pdf")
+par(mfrow=c(1,2))
+plot(mca2, axes = c(1,2), choix = "ind", invisible = "var", label="none", title = "Dim. 1 vs Dim. 2", col.ind="light gray")
+plot(mca2, axes = c(1,2), choix = "var", title = "Socio-economic variables\ncontribution", col.var = "black")
 dev.off()
-summary(mca3, scree = TRUE, rows = FALSE, columns = FALSE)
 
 #Standarization, socioeconomic score and deciles
-q <- scale(mca3$rowcoord[,1], center = T, scale = T)
+q <- scale(mca2$ind$coord[,1], center = T, scale = T)
+par(mfrow=c(1,1))
 hist(q)
 range01 <- function(x){(x-min(x))/(max(x)-min(x))}
 final <- nonamcch9[,c("folio","edad", "s5", "s6", "euh", "ien", "ytotcorh", "decilth", "quintilth", "cuartilth", "tercilth", "altobajoth")]
@@ -261,7 +277,7 @@ y <- final[,c("folio", "pse", "decilpse", "quintilpse", "cuartilpse", "tercilpse
 #Socioeconomic score
 summary(final$pse)
 sd(final$pse)
-png("Histpse.png", width = 4.9, height = 5.7, units = "in", res = 500)
+pdf("Histpse.pdf")
 hpse<-hist(final$pse, xlab="Socioeconomic score", ylab = "Frequency", main=NULL)
 dev.off()
 
@@ -269,33 +285,30 @@ dev.off()
 #Number of offspring
 summary(final$s5)
 sd(final$s5)
-png("Hists5.png", width = 4.9, height = 5.7, units = "in", res = 500)
+pdf("Hists5.pdf")
 hs5<-hist(final$s5, xlab="Number of offspring", ylab = "Frequency", main=NULL)
 dev.off()
 #Age at first birth
-summary(final$s6)
-sd(final$s6)
-png("Hists6.png", width = 4.9, height = 5.7, units = "in", res = 500)
-hs6<-hist(final$s6, xlab="Age at first birth", ylab = "Frequency", main= NULL)
+finals6 <- final[complete.cases(final$s6),]
+summary(finals6$s6)
+sd(finals6$s6)
+pdf("Hists6.pdf")
+hs6<-hist(finals6$s6, xlab="Age at first birth", ylab = "Frequency", main= NULL)
 dev.off()
 #age at last birth
-summary(final$euh)
-sd(final$euh)
-png("Histeuh.png", width = 4.9, height = 5.7, units = "in", res = 500)
-heuh<-hist(final$euh, xlab="Age last birth", ylab = "Frequency", main=NULL)
+finaleuh <- final[complete.cases(final$euh),]
+summary(finaleuh$euh)
+sd(finaleuh$euh)
+pdf("Histeuh.pdf")
+heuh<-hist(finaleuh$euh, xlab="Age last birth", ylab = "Frequency", main=NULL)
 dev.off()
 #Interbirth intervals
 finalien <- final[which(final$ien > 0),]
-test <- finalien[which(finalien$s5 > 1),]
-summary(test$ien)
-sd(test$ien)
-png("Histien.png", width = 4.9, height = 5.7, units = "in", res = 500)
-hien<-hist(final$ien, xlab="Interbirth intervals", ylab = "Frequency", main=NULL)
-dev.off()
-#summary plot of descriptive statistics
-png("boxplotlht.png", width = 4.9, height = 5.7, units = "in", res = 500)
-boxplot(final$s5, final$s6, final$euh, final$ien, xaxt="n", xlab="Life-history traits", main=NULL)
-axis(1, at=1:4, labels = c("N?Off", "AFR", "ALR", "IBI"))
+finalien <- finalien[which(finalien$s5 > 1),]
+summary(finalien$ien)
+sd(finalien$ien)
+pdf("Histien.pdf")
+hien<-hist(finalien$ien, xlab="Interbirth intervals", ylab = "Frequency", main=NULL)
 dev.off()
 
 #4.- Life-history traits~socioeconomic score analysis
@@ -313,6 +326,7 @@ library(fitdistrplus)
 #"qcc" package
 install.packages("qcc")
 library(qcc)
+#"MASS" package
 install.packages("MASS")
 library(MASS)
 
@@ -327,38 +341,41 @@ plot(fit.pse)
 #Life-history analysis
 #Number of offspring
 #Distribution
-descdist(final$s5, discrete = TRUE, boot = 500)
+descdist(residuals(mods5), discrete = FALSE, boot = 500)
 fit.s5 <- fitdist(final$s5, "pois")
 summary(fit.s5)
 png("Fits5.png", width = 4.9, height = 5.7, units = "in", res = 500)
 plot(fit.s5)
 dev.off()
 #Poisson regression
-mods5 <- glm(s5 ~ pse, family = poisson(), data = final)
+mods5 <- glm(s5 ~ pse, family = poisson, data = final)
 summary(mods5)
+par(mfrow=c(2,2))
+plot(mods5)
 coef(mods5)
 exp(coef(mods5))
 #Overdispersion testing
 qcc.overdispersion.test(final$s5, type="poisson")
 #Figure
-png("regs5.png", width = 4.9, height = 5.7, units = "in", res = 500)
-plot(final$pse, final$s5, xlab = "Socioeconomic score", ylab = "Number of offspring", main = NULL, col = (gray(0.3)))
-newx <- seq(min(final$pse), max(final$pse), length.out=6504)
+pdf("regs5.pdf")
+par(mfrow=c(1,1))
+plot(final$pse, final$s5, xlab = "Socioeconomic score", ylab = "Number of offspring", main = NULL, col = (gray(0.5)))
+newx <- seq(min(final$pse), max(final$pse), length.out=4766)
 predProbs<-predict(mods5,data.frame(pse=newx), type="response")
-lines(seq(min(final$pse), max(final$pse), length.out=6504), predProbs, col=("black"), lwd=2)
+lines(seq(min(final$pse), max(final$pse), length.out=4766), predProbs, col=("black"), lwd=2)
 dev.off()
 #Age at first birth
 #Variables definition
-evento1 <- final$s6
-grupo1.1 <- final$decilpse
+evento1 <- finals6$s6
+grupo1.1 <- finals6$decilpse
 grupo1.1 <- as.factor(grupo1.1)
-grupo1.2 <- final$quintilpse
+grupo1.2 <- finals6$quintilpse
 grupo1.2 <- as.factor(grupo1.2)
-grupo1.3 <- final$cuartilpse
+grupo1.3 <- finals6$cuartilpse
 grupo1.3 <- as.factor(grupo1.3)
-grupo1.4 <- final$tercilpse
+grupo1.4 <- finals6$tercilpse
 grupo1.4 <- as.factor(grupo1.4)
-grupo1.5 <- final$altobajose
+grupo1.5 <- finals6$altobajose
 grupo1.5 <- as.factor(grupo1.5)
 #Kaplan-Meier survival analysis
 kms5survival1 <- survfit(Surv(evento1) ~ 1)
@@ -368,66 +385,69 @@ title("Kaplan-Meier survival analysis for age first birth")
 #Kaplan-Meier survival analysis by socioeconomic group
 kms5survivalgse1.1 <- survfit(Surv(evento1) ~ grupo1.1)
 summary(kms5survivalgse1.1)
-png("surv10afb.png", width = 4.9, height = 5.7, units = "in", res = 500)
+pdf("surv10afb.png")
 plot(kms5survivalgse1.1, xlab = "Age at first birth", col = (gray(0:7/10)), lty = (1:10))
 legend("topright", ncol = 2, legend = c("decile 1", "decile 2", "decile 3", "decile 4", "decile 5", "decile 6", "decile 7", "decile 8", "decile 9", "decile 10"), col = gray(0:7/10), lty = (1:10), cex = 0.8)
 dev.off()
 kms5survivalgse1.2 <- survfit(Surv(evento1) ~ grupo1.2)
 summary(kms5survivalgse1.2)
-png("surv5afb.png", width = 4.9, height = 5.7, units = "in", res = 500)
+pdf("surv5afb.pdf")
 plot(kms5survivalgse1.2, xlab = "Age at first birth", col = gray(0:5/5), lty = (1:5))
 legend("topright", ncol = 2, legend = c("quintile 1", "quintile 2", "quintile 3", "quintile 4", "quintile 5"), col = gray(0:5/5), lty=(1:5), cex=0.8)
 dev.off()
 kms5survivalgse1.3 <- survfit(Surv(evento1) ~ grupo1.3)
 summary(kms5survivalgse1.3)
-png("surv4afb.png", width = 4.9, height = 5.7, units = "in", res = 500)
+pdf("surv4afb.pdf")
 plot(kms5survivalgse1.3, xlab = "Age at first birth", col = gray(0:4/4), lty=(1:4))
 legend("topright", ncol = 2, legend = c("quartile 1", "quartile 2", "quartile 3", "quartile 4"), col = gray(0:4/4), lty=(1:4), cex=0.8)
 dev.off()
 kms5survivalgse1.4 <- survfit(Surv(evento1) ~ grupo1.4)
 summary(kms5survivalgse1.4)
-png("surv3afb.png", width = 4.9, height = 5.7, units = "in", res = 500)
+pdf("surv3afb.pdf")
 plot(kms5survivalgse1.4, xlab = "Age at first birth", col = gray(0:3/3), lty=(1:3))
 legend("topright", legend = c("tertile 1", "tertile 2", "tertile 3"), col = gray(0:3/3), lty=(1:3), cex=0.8)
 dev.off()
 kms5survivalgse1.5 <- survfit(Surv(evento1) ~ grupo1.5)
 summary(kms5survivalgse1.5)
-png("surv2afb.png", width = 4.9, height = 5.7, units = "in", res = 500)
+pdf("surv2afb.pdf")
 plot(kms5survivalgse1.5, xlab = "Age at first birth", col = gray(0:2/2), lty=(1:2))
 legend("topright", legend = c("Low SES", "High SES"), col = gray(0:2/2), lty=(1:2), cex=0.8)
 dev.off()
 #Distribution
-descdist(final$s6, discrete = TRUE, boot = 500)
-fit.s6 <- fitdist(final$s6, "nbinom")
+descdist(finals6$s6, discrete = TRUE, boot = 500)
+fit.s6 <- fitdist(finals6$s6, "nbinom")
 summary(fit.s6)
-png("Fits6.png", width = 4.9, height = 5.7, units = "in", res = 500)
+pdf("Fits6.pdf")
 plot(fit.s6)
 dev.off()
 #Negative binomial regression
-mods6 <- glm.nb(s6 ~ pse, data = final)
+mods6 <- glm.nb(s6 ~ as.vector(pse), data = finals6)
 summary(mods6)
+par(mfrow=c(2,2))
+plot(mods6)
 coef(mods6)
 exp(coef(mods6))
 #Figure
-png("regs6.png", width = 4.9, height = 5.7, units = "in", res = 500)
-plot(final$pse, final$s6, xlab = "Socioeconomic score", ylab = "Age at first birth", main = NULL, col = gray(0.3))
-newx1 <- seq(min(final$pse), max(final$pse), length.out=6504)
+pdf("regs6.pdf")
+par(mfrow=c(1,1))
+plot(finals6$pse, finals6$s6, xlab = "Socioeconomic score", ylab = "Age at first birth", main = NULL, col = gray(0.5))
+newx1 <- seq(min(finals6$pse), max(finals6$pse), length.out=4496)
 predProbs1<-predict(mods6,data.frame(pse=newx1), type="response")
-lines(seq(min(final$pse), max(final$pse), length.out=6504), predProbs1, col=1, lwd=2)
+lines(seq(min(finals6$pse), max(finals6$pse), length.out=4496), predProbs1, col=1, lwd=2)
 dev.off()
 
 #Age at last birth
 #Variables definition
-evento2 <- final$euh
-grupo2.1 <- final$decilpse
+evento2 <- finaleuh$euh
+grupo2.1 <- finaleuh$decilpse
 grupo2.1 <- as.factor(grupo2.1)
-grupo2.2 <- final$quintilpse
+grupo2.2 <- finaleuh$quintilpse
 grupo2.2 <- as.factor(grupo2.2)
-grupo2.3 <- final$cuartilpse
+grupo2.3 <- finaleuh$cuartilpse
 grupo2.3 <- as.factor(grupo2.3)
-grupo2.4 <- final$tercilpse
+grupo2.4 <- finaleuh$tercilpse
 grupo2.4 <- as.factor(grupo2.4)
-grupo2.5 <- final$altobajose
+grupo2.5 <- finaleuh$altobajose
 grupo2.5 <- as.factor(grupo2.5)
 #Kaplan-Meier survival analysis
 kms5survival2 <- survfit(Surv(evento2) ~ 1)
@@ -437,68 +457,74 @@ title("Kaplan-Meier survival analysis for \nage at last birth")
 #Kaplan-Meier survival analysis by socioeconomic groups
 kms5survivalgse2.1 <- survfit(Surv(evento2) ~ grupo2.1)
 summary(kms5survivalgse2.1)
-png("surv10euh.png", width = 4.9, height = 5.7, units = "in", res = 500)
+pdf("surv10euh.pdf")
 plot(kms5survivalgse2.1, xlab = "Age at last birth", col = gray(0:7/10), lty=(1:10))
 legend("bottomleft", ncol = 2, legend = c("decile 1", "decile 2", "decile 3", "decile 4", "decile 5", "decile 6", "decile 7", "decile 8", "decile 9", "decile 10"), col = gray(0:7/10), lty=(1:10), cex = 0.8)
 dev.off()
 kms5survivalgse2.2 <- survfit(Surv(evento2) ~ grupo2.2)
 summary(kms5survivalgse2.2)
-png("surv5euh.png", width = 4.9, height = 5.7, units = "in", res = 500)
+pdf("surv5euh.pdf")
 plot(kms5survivalgse2.2, xlab = "Age at last birth", col = gray(0:5/5), lty=(1:5))
 legend("bottomleft", ncol = 2, legend = c("quintile 1", "quintile 2", "quintile 3", "quintile 4", "quintile 5"), col = gray(0:5/5), lty = (1:5), cex=0.8)
 dev.off()
 kms5survivalgse2.3 <- survfit(Surv(evento2) ~ grupo2.3)
 summary(kms5survivalgse2.3)
-png("surv4euh.png", width = 4.9, height = 5.7, units = "in", res = 500)
+pdf("surv4euh.pdf")
 plot(kms5survivalgse2.3, xlab = "Age at last birth", col = gray(0:4/4), lty=(1:4))
 legend("bottomleft", ncol = 2, legend = c("quartile 1", "quartile 2", "quartile 3", "quartile 4"), col = gray(0:4/4), lty=(1:4), cex=0.8)
 dev.off()
 kms5survivalgse2.4 <- survfit(Surv(evento2) ~ grupo2.4)
 summary(kms5survivalgse2.4)
-png("surv3euh.png", width = 4.9, height = 5.7, units = "in", res = 500)
+pdf("surv3euh.pdf")
 plot(kms5survivalgse2.4, xlab = "Age at last birth", col = gray(0:3/3), lty=(1:3))
 legend("bottomleft", legend = c("tertile 1", "tertile 2", "tertile 3"), col = gray(0:3/3), lty=(1:3), cex=0.8)
 dev.off()
 kms5survivalgse2.5 <- survfit(Surv(evento2) ~ grupo2.5)
 summary(kms5survivalgse2.5)
-png("surv2euh.png", width = 4.9, height = 5.7, units = "in", res = 500)
+pdf("surv2euh.pdf")
 plot(kms5survivalgse2.5, xlab = "Age at last birth", col = gray(0:2/2), lty = (1:2))
 legend("bottomleft", legend = c("Low SES", "High SES"), col = gray(0:2/2), lty = (1:2), cex=0.8)
 dev.off()
 #Distribution
-descdist(final$euh, discrete = TRUE, boot = 500)
-fit.euh <- fitdist(final$euh, "norm")
+descdist(finaleuh$euh, discrete = TRUE, boot = 500)
+fit.euh <- fitdist(finaleuh$euh, "norm")
 summary(fit.euh)
-png("Fiteuh.png", width = 4.9, height = 5.7, units = "in", res = 500)
+pdf("Fiteuh.pdf")
 plot(fit.euh)
 dev.off()
 #Linear regression
-modeuh <- lm(euh ~ pse, data = final)
+modeuh <- lm(euh ~ as.vector(pse), data = finaleuh)
 summary(modeuh)
+par(mfrow=c(2,2))
+plot(modeuh)
 coef(modeuh)
 #Figure
-png("regeuh.png", width = 4.9, height = 5.7, units = "in", res = 500)
-plot(final$pse, final$euh, xlab = "Socioeconomic score", ylab = "Age at last birth", main = NULL, col=gray(0.3))
-newx2 <- seq(min(final$pse), max(final$pse), length.out=6504)
+pdf("regeuh.pdf")
+par(mfrow=c(1,1))
+plot(finaleuh$pse, finaleuh$euh, xlab = "Socioeconomic score", ylab = "Age at last birth", main = NULL, col=gray(0.5))
+newx2 <- seq(min(finaleuh$pse), max(finaleuh$pse), length.out=4496)
 predProbs2<-predict(modeuh,data.frame(pse=newx2), type="response")
-lines(seq(min(final$pse), max(final$pse), length.out=6504), predProbs2, col=1, lwd=2)
+lines(seq(min(finaleuh$pse), max(finaleuh$pse), length.out=4496), predProbs2, col=1, lwd=2)
 dev.off()
 
 #Interbirth intervals
 #Distribution
-descdist(test$ien, discrete = FALSE, boot = 500)
-fit.ien <- fitdist(test$ien, "gamma")
-png("Fitien.png", width = 4.9, height = 5.7, units = "in", res = 500)
+descdist(finalien$ien, discrete = FALSE, boot = 500)
+fit.ien <- fitdist(finalien$ien, "gamma")
+pdf("Fitien.pdf")
 plot(fit.ien)
 dev.off()
 #Gamma regresion
-modien <- glm(ien ~ pse, family = Gamma(), data = test)
+modien <- glm(ien ~ as.vector(pse), family = Gamma(), data = finalien)
 summary(modien)
+par(mfrow=c(2,2))
+plot(modien)
 exp(coef(modien))
 #Figure
-png("regien.png", width = 4.9, height = 5.7, units = "in", res = 500)
-plot(test$pse, test$ien, xlab = "Socioeconomic score", ylab = "Interbirth intervals", main = NULL, col=gray(0.3))
-newx3 <- seq(min(test$pse), max(test$pse), length.out=5791)
+pdf("regien.pdf")
+par(mfrow=c(1,1))
+plot(finalien$pse, finalien$ien, xlab = "Socioeconomic score", ylab = "Interbirth intervals", main = NULL, col=gray(0.5))
+newx3 <- seq(min(finalien$pse), max(finalien$pse), length.out=4766)
 predProbs3<-predict(modien,data.frame(pse=newx3), type="response")
-lines(seq(min(test$pse), max(test$pse), length.out=5791), predProbs3, col=1, lwd=2)
+lines(seq(min(finalien$pse), max(finalien$pse), length.out=4766), predProbs3, col=1, lwd=2)
 dev.off()
